38f29121af4a0c4fa9d78291f3ad07b2e49f46f5
takuo_ishida <archersparadox.100@gmail.com>
2020-12-30 17:23:59 +0900

    props test is OK

--- src/App.vue
+++ src/App.vue
@@ -9,35 +9,49 @@
     </p>
     <input type="text" v-model="msg">
     <button @click="clear()">clear</button>
+    <p>{{counter}}</p>
+    <child-component :name="name"></child-component>
   </div>
 </template>
 
 <script>
 import myheader from './components/myheader'
+import ChildComponent from './components/ChildComponent'
 
 export default {
   components: {
-    myheader
+    // どちらも同じ。'myheader'はテンプレート名を上書きできる
+    myheader,
+    // 'myheader': myheader
+    ChildComponent
   },
-  data () {
+  data() {
     return {
-      msg: "hello"
+      msg: "hello",
+      // Chileコンポネントでpropsとして受け取る
+      name: "Kei",
+      counter: 0
     }
   },
   methods: {
     clear() {
       this.msg = ""
     }
+  },
+  // 初期読み込み時にAPIから取得する
+  created() {
+    fetch('http://www.geonames.org/postalCodeLookupJSON?postalcode=10504&country=US')
+    .then( res => {
+      return res.json()
+    })
+    .then( json => {
+      this.msg = json.postalcodes[0].adminName1
+    })
+    .catch( (err) => {
+      this.msg = err // error message
+    })
   }
 }
-// import HelloWorld from './components/HelloWorld.vue'
-
-// export default {
-//   name: 'App',
-//   components: {
-//     HelloWorld
-//   }
-// }
 </script>
 
 <style>
